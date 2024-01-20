a55c8f6f86aa6232d9f2e77362b96c9cc46dcac7
ganu0811 <ganufindsu@gmail.com>
2024-01-20 14:34:51 +0000

    Add setup.py file with project details and dependencies

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Ganesh'
+    author_email='ganufindsu@gmail.com'
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas','seaborn']
+
+)
