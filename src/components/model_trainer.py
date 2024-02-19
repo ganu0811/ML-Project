94cd2e7c517f59f054011d3cb6b943d3499d4732
ganu0811 <ganufindsu@gmail.com>
2024-02-19 21:14:38 +0000

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -51,8 +51,44 @@ class ModelTrainer:
                 "XGBoost":XGBRegressor(),
                 "CatBoost":CatBoostRegressor()
             }
+            params={
+                "DecisonTree":{
+                    "criterion":["squared_error","friedman_mse","absolute_error","poisson"],
+                     "splitter":["best","random"],
+                    "max_features":["auto","sqrt","log2"]
+                },
+                'RandomForest':{
+                    'criterion':["squared_error","friedman_mse","absolute_error","poisson"],
+                    'max_features':["auto","sqrt","log2"],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'GradientBoosting':{
+                    'loss':["squared_error","absolute_error","huber","quantile"],
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'criterion':['squared_error','friedman_mse',],
+                    'max_features':['auto','sqrt','log2'],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'LinearRegression':{},
+                'XGBRegressor':{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'CatBoostRegressor':{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.1,0.01,0.05],
+                    'iterations':[30,50,100]
+                },
+                AdaBoostRegressor:{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }   
+                
             
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
             
             ## To get the best model from the model report
             
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,14 +22,21 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
             
-            model.fit(X_train,y_train) # Train the model
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            
+            # model.fit(X_train,y_train) # Train the model
             
             y_train_pred=model.predict(X_train) # Predict the training data
             
@@ -44,5 +52,14 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e,sys)
-        
+    
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+    
             
